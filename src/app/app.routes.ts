58f06b3400281aec6427a4879da4627e2e58a8be
roshh-a <roshini.ananth@taffinc.com>
2024-08-28 18:56:57 +0530

    about and home added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        title: 'Home',
+        component: HomeComponent
+    },
+    {
+        path: 'about',
+        title: 'About us',
+        component: AboutComponent
+    }
+];
