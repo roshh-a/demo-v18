f219589e1d0822c20f39d7a16ad9efa781814fb0
roshh-a <roshini.ananth@taffinc.com>
2024-08-28 19:14:30 +0530

    added contact routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 export const routes: Routes = [
     {
@@ -12,5 +13,10 @@ export const routes: Routes = [
         path: 'about',
         title: 'About us',
         component: AboutComponent
+    },
+    {
+        path: 'contact-us',
+        title: 'Contact us',
+        component: ContactComponent
     }
 ];
