3b15396367f40878d0b0da1e78d03fc20cf75c5e
roshh-a <roshini.ananth@taffinc.com>
2024-08-30 20:31:04 +0530

    pages along with signal implemention

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RouterLink } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterLink],
+  imports: [RouterOutlet, RouterLink, HeaderComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent extends HomeComponent{
   title = 'demo-v18';
+
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
-
+import { Component, computed, Signal, signal } from '@angular/core';
+import { HeaderComponent } from '../../shared/header/header.component';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  public dataForm: any = FormGroup;
+  public username = signal('name');
+  // firstValue: number = 0;
+  // secondValue: number = 0;
+  // public value1 = signal(0);
+  // total: number = 0;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.dataForm = this.formBuilder.group({
+      // firstVal: ['', Validators.required],
+      // secondVal: ['', Validators.required]
+      name: ['', Validators.required]
+    });
+  }
+  onSubmit($event: any) {
+    // $event.preventDefaults();
+    if (this.dataForm.valid) {
+      this.username.update(() => this.dataForm.value.name);
+      // this.firstValue = parseInt(this.dataForm.value.firstVal);
+      // this.secondValue = parseInt(this.dataForm.value.secondVal);
+      // let value1 = signal(this.firstValue);
+      // let value2 = signal(this.secondValue);
+      // this.total = value1() + value2();
+      // console.log(this.total);
+      // this.total = this.finalVal.update(this.total => this.total + 3);
+      // window.alert(this.finalVal);
+    }
+  }
 }
